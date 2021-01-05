cd74a9de3bd9af48e225de10612f0094a3e3a3b5
Mykhailo Shevchenko <mykheilsh@gmail.com>
2021-01-05 23:48:35 +0100

    Changed public: and private: order, added MaximaImpl for copy and swap with pimpl idioms

--- src/function_maxima.h
+++ src/function_maxima.h
@@ -5,42 +5,68 @@
 #include <memory>
 #include <set>
 
+template <typename A, typename V>
+class point_type {
+  public:
+    // Returns function argument.
+    A const &arg() const {
+        return ptr.get()->first;
+    }
+
+    // Returns function value at a given point.
+    V const &value() const {
+        return ptr.get()->second;
+    }
+
+    // Can throw exception, is this ok then?
+    point_type(const point_type &other) :
+        arg_(make_shared(other.arg())), val_(make_shared(other.value())) {}
+
+    // Exceptions safety? Is here a memory leak?
+    point_type &operator=(const point_type &other) {
+        arg_ = make_shared(other.arg());
+        val_ = make_shared(other.value());
+    }
+
+  private:
+    std::shared_ptr<A> arg_;
+    std::shared_ptr<V> val_;
+
+    friend point_type FunctionMaxima::make_point(
+        std::shared_ptr<A>, std::shared_ptr<V>
+    );
+
+    explicit point_type(std::shared_ptr<A> arg, std::shared_ptr<V> val) :
+        arg_(arg), val_(val) {};
+};
+
 template<typename A, typename V>
 class FunctionMaxima {
+  public:
+    using iterator = typename std::multiset<point_type>::const_iterator;
+    using mx_iterator = typename std::multiset<point_type>::const_iterator;
+    using size_type = typename std::multiset<point_type>::size_type;
+
+    FunctionMaxima() = default;
+    FunctionMaxima(const FunctionMaxima &other) = default;
+    ~FunctionMaxima() = default;
+
+    FunctionMaxima &operator=(FunctionMaxima other) {
+        other.swap(*this);
+        return *this;
+    }
+
+    iterator begin() const { return imp->begin(); }
+    iterator end() const { return imp->end(); }
+    iterator find(A const &x) const { return imp->find(x); }
+    mx_iterator mx_begin() const { return imp->mx_begin(); }
+    mx_iterator mx_end() const { return imp->mx_end(); }
+    size_type size() const { return imp->size(); }
+    V const &value_at(const A &a) const { return imp->value_at(a); }
+    void set_value(const A &a, const V &v) { imp->set_value(a, v); }
+    void erase(const A &a) { imp->erase(a); }
+
   private:
-    //using Point = std::pair<A, V>;
-
-    class point_type {
-      private:
-        std::shared_ptr<A> arg_;
-        std::shared_ptr<V> val_;
-
-        friend point_type FunctionMaxima::make_point(std::shared_ptr<A>, std::shared_ptr<V>);
-
-        explicit point_type(std::shared_ptr<A> arg, std::shared_ptr<V> val) :
-            arg_(arg), val_(val) {};
-
-      public:
-        // Returns function argument.
-        A const &arg() const {
-            return ptr.get()->first;
-        }
-
-        // Returns function value at a given point.
-        V const &value() const {
-            return ptr.get()->second;
-        }
-
-        // Can throw exception, is this ok then?
-        point_type(const point_type &other) : arg_(make_shared(other.arg())), val_(make_shared(other.value())) {}
-
-        // Exceptions safety? Is here a memory leak?
-        point_type &operator=(const point_type &other) {
-            arg_ = make_shared(other.arg());
-            val_ = make_shared(other.value());
-        }
-    };
-
     class point_type_comparator_by_arg {
         bool operator()(const point_type &p1, const point_type &p2) {
             return p1.arg() < p2.arg();
@@ -60,62 +86,47 @@ class FunctionMaxima {
         return point_type(arg, value);
     }
 
-    std::multiset<point_type, point_type_comparator_by_arg> points;
-    std::multiset<point_type, point_type_comparator_by_value> mx_points;
+    template <typename A, typename V>
+    class MaximaImpl {
+      public:
+        MaximaImpl() = default;
+        ~MaximaImpl() = default;
 
-  public:
-    using iterator = typename std::multiset<point_type>::const_iterator;
-    using mx_iterator = typename std::multiset<point_type>::const_iterator;
-    using size_type = typename std::multiset<point_type>::size_type;
-
-    FunctionMaxima() = default;
-    ~FunctionMaxima() = default;
-
-    FunctionMaxima(const FunctionMaxima &other) : points(other.points) {
-        for (auto &pt : other.mx_points) {
-            // fishy
-            mx_points.insert(*points.find(pt));
+        MaximaImpl(const MaximaImpl &other) : points(other.points) {
+            for (auto &pt : other.mx_points) {
+                // fishy
+                mx_points.insert(*points.find(pt));
+            }
         }
-    }
 
-    FunctionMaxima &operator=(FunctionMaxima other) {
-        // TODO pimpl, to create nothrow swap
-        other.swap(*this);
-        return *this;
-    }
+        iterator find(A const &x) const {
+            // Possible because A is guaranteed to have a copy constructor.
+            std::shared_ptr<A> A_ptr = make_shared<A>(x);
+            point_type pt = make_point(A_ptr, nullptr);
+            return points.find(pt);
+        }
 
-    iterator begin() const {
-        return points.begin();
-    }
+        iterator begin() const { return points.begin(); }
+        iterator end() const { return points.end(); }
+        mx_iterator mx_begin() const { return mx_points.begin(); }
+        mx_iterator mx_end() const { return  mx_points.end(); }
+        size_type size() const { return points.size(); }
 
-    iterator end() const {
-        return points.end();
-    }
+        V const &value_at(const A &a) const {
+        }
 
-    iterator find(A const &x) const {
-        // Possible because A is guaranteed to have a copy constructor.
-        std::shared_ptr<A> A_ptr = make_shared<A>(x);
-        point_type pt = make_point(A_ptr, nullptr);
-        return points.find(pt);
-    }
+        void set_value(const A &a, const V &v) {
+        }
 
-    mx_iterator mx_begin() const {
-        return mx_points.begin();
-    }
-  
-    mx_iterator mx_end() const {
-        return  mx_points.end();
-    }
+        void erase(const A &a) {
+        }
 
-    size_type size() const {
-        return points.size();
-    }
+      private:
+        std::multiset<point_type, point_type_comparator_by_arg> points;
+        std::multiset<point_type, point_type_comparator_by_value> mx_points;
+    };
 
-    // set_value mutator
-
-    // erase mutator
-
-    // value_at inspector
+    std::unique_ptr<MaximaImpl<A, V>> imp;
 };
 
 #endif /* FUNCTION_MAXIMA_H */

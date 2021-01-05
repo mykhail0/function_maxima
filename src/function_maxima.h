a6e5c520957f98e15133488c6d006f6d5015560a
j-szulc <james216@wp.pl>
2021-01-05 03:56:15 +0100

    Added comparators and make_point

--- src/function_maxima.h
+++ src/function_maxima.h
@@ -1,105 +1,100 @@
 #ifndef FUNCTION_MAXIMA_H
 #define FUNCTION_MAXIMA_H
 
-/*
-class MapIterator {
-  private:
-  	iterator z mapy
-  operator*() {
-    
-  }
-}
+#include <utility>
+#include <memory>
+#include <set>
 
-pair<A, V> --> point_type
-*/
+using namespace std;
 
-
-
-template <typename A, typename V>
+template<typename A, typename V>
 class FunctionMaxima {
-  private:
-  	using Point = pair<A, V>;
-  
-    class point_type {
-      private:
-    		shared_ptr<Point> ptr;
+private:
+    using Point = pair<A, V>;
 
-      	friend point_type FunctionMaxima::make_point(A, V);
-        point_type(Point* ptr) : ptr(ptr) = default;
-      public:
+    class point_type {
+    private:
+        shared_ptr<Point> ptr;
+
+        friend point_type FunctionMaxima::make_point(A, V);
+
+        explicit point_type(Point *ptr) : ptr(ptr) {};
+
+    public:
         // Zwraca argument funkcji.
-        A const& arg() const {
-          	return ptr.get()->first;
+        A const &arg() const {
+            return ptr.get()->first;
         }
 
         // Zwraca wartość funkcji w tym punkcie.
-        V const& value() const {
-          	return ptr.get()->newsecond;
+        V const &value() const {
+            return ptr.get()->second;
         }
     };
 
+    class point_type_comparator_by_arg {
+        bool operator()(const point_type &p1, const point_type &p2) {
+            return p1.arg() < p2.arg();
+        }
+    };
 
-  MS 1) klasa do testowania exception
-  JS 1) comparator set
-  JS 1) destruktor
-  JS 1) point_type, make_point
+    class point_type_comparator_by_value {
+        bool operator()(const point_type &p1, const point_type &p2) {
+            return p1.value() < p2.value();
+        }
+    };
 
-    // copy and swap idiom
-    // non throw swap idiom
-    constr copy tmp(rhs)
-    non throw swap (this, tmp)
-  MS 2) konstruktor kopia, operator= --- strong guarantee
+    point_type make_point(A arg, V value) {
+        return point_type(new Point(arg, value));
+    }
 
-  erase, set_value
-  find, value_at
+    set<point_type, point_type_comparator_by_arg> points;
+    set<point_type, point_type_comparator_by_value> mx_points;
 
-
-  /// 1111
-  point_type make_point(A,V);
-  set<point_type> points;
-  set<point_type> mx_points;
-
-  public:
-  	using mx_iterator = set::const_iterator;
-  	using iterator = set::const_iterator;
-  	using size_type = set::size_type;
+public:
+    using iterator = typename set<point_type>::const_iterator;
+    using mx_iterator = typename set<point_type>::const_iterator;
+    using size_type = typename set<point_type>::size_type;
 
     FunctionMaxima() = default;
-    FunctionMaxima(const FunctionMaxima&);
-		FunctionMaxima &operator=(const FunctionMaxima&);
-    ~FunctionMaxima();
 
-		iterator begin() const {
-      return points.begin();
+    FunctionMaxima(const FunctionMaxima &);
+
+    FunctionMaxima &operator=(const FunctionMaxima &);
+
+    ~FunctionMaxima() = default;
+
+    iterator begin() const {
+        return points.begin();
     }
 
-  	iterator end() const {
-      return points.end();
+    iterator end() const {
+        return points.end();
     }
 
-  	iterator find(A const& x) const {
-      // TODO strong exception
-      try {
-        point_type pt = make_point(x, static_cast<V>(nullptr));
-      }
-    	return points.find(pt);
+    iterator find(A const &x) const {
+        // TODO strong exception
+        unique_ptr<V> dummy(new V);
+        point_type pt = make_point(x, *dummy);
+        return points.find(pt);
     }
 
-		mx_iterator mx_begin() const {
-      return mx_points.begin();
+    mx_iterator mx_begin() const {
+        return mx_points.begin();
     }
   
   	mx_iterator mx_end() const {
      return  mx_points.end();
     }
-  
-  set value mutator
 
-    erase mutator
+    //set value mutator
 
-    value at inspector
-    
-    size()
+    //erase mutator
+
+    //value at inspector
+
+    //size()
 };
+
 
 #endif /* FUNCTION_MAXIMA_H */
